cf8717419d1153e9ca015d8244315b93e3454614
SamuelFerreira280 <127429069+SamuelFerreira280@users.noreply.github.com>
2023-06-02 11:41:15 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,6 @@
+{
+  {"id":1, "Nome": "Sabão em Pó", "marca": "OMO", "preço": "12.90" },
+    {"id":2, "Nome": "Sabonete", "marca": "LUX", "preço": "5.90" },
+      {"id":3, "Nome": "chanpoo", "marca": "palmolive", "preço": "8.90" }
+    
+}
